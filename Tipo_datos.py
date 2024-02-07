c78cb562ffc1aba877097ac39a308f311f12baa0
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-06 21:30:46 -0500

    Commit_02

--- Tipo_datos.py
+++ Tipo_datos.py
@@ -7,3 +7,47 @@ print("Tipo cadena")
 print(type("Hola"))
 print("Tipo booleano")
 print(type(True))
+
+#ejemplos con las cadenas
+print("Hola, "+" Amigos")
+print("Saludos"*4)
+variable = "cadena en variable"
+print(variable)
+variable = "sumo esto a " + variable
+print(variable)
+
+#imprimir la posicion 3 de la cadena
+print(variable[3])
+
+#imprimir la posicion 5 de la cadena
+print(variable[5])
+
+#imprimir la ultima posicion de la cadena
+print(variable[-1])
+
+#imprimir subcadena desde la posicion 2 hasta la 5 (la posicion 5 no se incluye)
+print(variable[2:5])
+
+#imprimir longitud de la variable (se cuentan espacios en blanco)
+print(len(variable))
+
+#imprimir la cadena en mayuscula
+print(variable.upper())
+
+#imprimir la cadena en minuscula
+print(variable.lower())
+
+#imprimir la cadena con el primer caracter en mayuscula
+print(variable.capitalize())
+
+cadena = "    Esto es una cadena con muchos espacios    "
+
+print(cadena)
+
+#imprimir la cadena sin los espacios por delante y por detras
+print(cadena.strip())
+
+#reemplazar valor en cadena
+cadena = "Esto es un texto sin reemplazar"
+print(cadena)
+print(cadena.replace("sin reemplazar", "con reemplazo"))
