4db0f941309319b640e3daddbc0b6e7f2e748654
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-07 18:56:55 -0500

    Commit_02

---
+++ Operadores.py
@@ -0,0 +1,60 @@
+print('Suma')
+print(1+6)
+
+print('Resta')
+print(1-6)
+
+print('Multiplicacion')
+print(2*9)
+
+print('Division')
+print(10/3)
+
+print('Division entera')
+print(10//3)
+
+print('Potencia')
+print(2**2)
+
+print('Residuo')
+print(8%4)
+
+print(1<3)
+
+print(4>10)
+
+print(5!=-5)
+
+print(2>=2)
+
+print(0==0)
+
+print(-1<1 and 1 <=1)
+
+print(5>1 or 2==-1)
+
+print('hola', 'Jose')
+
+print('hola '*4)
+
+print('hola', str(5))
+
+print('hola' , 5)
+
+print(int('3456'))
+
+print(type(int('3456')))
+
+print(type(str(456)))
+
+print(float(88))
+
+print(bool(1))
+
+print(bool(0))
+
+print(bool(""))
+
+print(bool(99))
+
+print(bool("False"))
