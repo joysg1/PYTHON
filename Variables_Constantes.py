ae2503fe1ac233d76455a0ed64943bee9eb16d2a
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-07 19:27:19 -0500

    Commit_03

---
+++ Variables_Constantes.py
@@ -0,0 +1,53 @@
+#damos valor a variable
+
+precio = 225
+cantidad = 3
+nombre = 'Joy'
+
+#calcular total
+
+total = precio * cantidad 
+print('Usuario: ', nombre)
+print('Precio: ', precio)
+print('Cantidad ', cantidad)
+print('Total: ', total)
+
+#borrar variable
+
+#del cantidad
+#print(cantidad)
+
+print("-------------------------------------------")
+
+#asignar otros valores 
+
+precio = 100
+cantidad = 23
+
+total = precio * cantidad
+
+#imprimir nuevo total
+
+total = precio * cantidad 
+print('Usuario: ', nombre)
+print('Precio: ', precio)
+print('Cantidad ', cantidad)
+print('Total: ', total)
+
+
+#creacion de una constante, por convencion su nombre va en mayuscula
+
+PASSWORD = '12345'
+
+#asignar varios valores a variables distintas
+
+a, b, c, d = 1, 0.5, "palabra", False
+
+print(a, b, c, d)
+
+#asignar un mismo valor a varias variables
+
+x = y = z = 71
+
+print(x, y, z)
+
