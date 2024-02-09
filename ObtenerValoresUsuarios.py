f6d6632f2e4192c163adddd3d4b160aa88ba4ece
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-08 20:40:12 -0500

    Commit_04

---
+++ ObtenerValoresUsuarios.py
@@ -0,0 +1,11 @@
+nombre = input('Por favor ingresa tu nombre: ')
+print("Hola" , "\t" , nombre)
+
+num1 = int(input('Por favor ingresa un numero: '))
+num2 = int(input('Por favor ingresa un segundo numero: '))
+
+print('El primer numero ingresado es ',num1, "\n")
+print('El segundo numero ingresado es ',num2, "\n")
+
+resultado = num1 + num2 
+print(f"La suma de {num1} mas {num2} es igual a {resultado}", "\n")
