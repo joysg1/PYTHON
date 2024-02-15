3c7b5cd0d01b45ea395cd99bb320b0da4e20cbbf
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-15 18:09:25 -0500

    Commit_18

---
+++ ComprensionListas_04.py
@@ -0,0 +1,9 @@
+numeros = [2,4,6,7,8]
+
+mayores_a_cinco = []
+
+mayores_a_cinco = [numero for numero in numeros if numero >5]
+
+print(f"Lista original de numeros {numeros}\n")
+print("*"*40)
+print(f"Lista de numeros mayores a cinco {mayores_a_cinco}\n")
