0b4d4dfa5097ab25a93d2f85638b7472027d47f2
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-17 19:34:48 -0500

    Commit_23

---
+++ FuncionesUtiles.py
@@ -0,0 +1,24 @@
+#redondeo
+print(round(8.67))
+print(round(3.4))
+
+#valor absoluto
+print(abs(-7))
+print(abs(-11))
+
+
+lista = [1,6,8,5,2,44,22,78,21]
+
+#minimo de la lista
+print(f"Lista original {lista}")
+print(f"Minimo de la lista: {min(lista)}")
+
+#maximo de la lista
+print(f"Maximo de la lista: {max(lista)}")
+
+#ordenar la lista
+print(f"Lista ordenada: {sorted(lista)}")
+
+#suma total de la lista
+print(f"Suma de los elementos de la lista {sum(lista)}")
+
