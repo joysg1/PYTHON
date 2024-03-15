e2aa3ca459d181544e0a9a6383756709ef293da5
joysg1 <joy.nelaton@utp.ac.pa>
2024-03-14 21:28:19 -0500

    Commit_59

---
+++ AyudaPython.py
@@ -0,0 +1,38 @@
+# help(print)
+# help(open)
+
+'''
+Documentacion del script
+
+'''
+
+
+"""
+Documentacion del script
+
+"""
+
+
+# Para documentar en bloque podemos usar comillas simples o dobles
+
+
+class Vehiculo:
+    
+ """
+
+ Documentacion de la clase Vehiculo 
+
+ """
+    
+pass
+
+
+# ejecutar en terminal para ver la ayuda (documentacion) de la clase:
+
+# from AyudaPython import Vehiculo
+
+# help(Vehiculo)
+
+
+
+
