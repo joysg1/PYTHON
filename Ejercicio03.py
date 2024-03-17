b86cbdf14b0a7ad95cb357ffe20e29f9a3bf2324
joysg1 <joy.nelaton@utp.ac.pa>
2024-03-16 20:47:40 -0500

    Commit_64

---
+++ Ejercicio03.py
@@ -0,0 +1,12 @@
+cadena = 'Hola'
+numero = 77
+
+print(f"Variable tipo cadena = {cadena}")
+print(f"Comprobacion de tipo = {type(cadena)}")
+print("*"*80)
+print("\n")
+
+print(f"Variable tipo numero = {numero}")
+print(f"Comprobacion de tipo = {type(numero)}")
+print("*"*80)
+print("\n")
---
+++ Ejercicio04.py
@@ -0,0 +1,39 @@
+
+'''
+
+cadena = 'Hola'
+numero = 77
+
+print(f"Variable tipo cadena = {cadena}")
+print(f"Comprobacion de tipo = {type(cadena)}")
+print("*"*80)
+print("\n")
+
+print(f"Variable tipo numero = {numero}")
+print(f"Comprobacion de tipo = {type(numero)}")
+print("*"*80)
+print("\n")
+
+
+# Procedemos a modificar las variables
+print("A continuacion procederemos a modificar los valores")
+cadena = str(input("Por favor ingrese el nuevo valor de la variable cadena: "))
+numero = int(input("Por favor ingrese el nuevo valor de la variable numero: "))
+print("*"*80)
+print("\n")
+
+print("---- Valores actualizados ----")
+print(f"Variable tipo cadena = {cadena}")
+print(f"Comprobacion de tipo = {type(cadena)}")
+print("*"*80)
+print("\n")
+
+print(f"Variable tipo numero = {numero}")
+print(f"Comprobacion de tipo = {type(numero)}")
+print("*"*80)
+print("\n")
+
+'''
+
+
+print("Hola"*5)
