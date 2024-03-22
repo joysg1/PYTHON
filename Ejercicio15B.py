24bf33dbb69ca45af1e7a459b0f102620b854348
Joy <joy.nelaton@utp.ac.pa>
2024-03-22 11:22:23 -0500

    Commit_81

--- Ejercicio15B.py
+++ Ejercicio15B.py
@@ -1,11 +1,13 @@
 #Archivo principal (empleado para llamar al modulo con las funciones)
 
-import Ejercicio15 as Operacion  # Importe del Ejercicio15 que es el archivo modulo (aquel que tiene las funciones)
+import Ejercicio15 as Operacion  
+"""
+Importe del Ejercicio15 que es el 
+archivo modulo (aquel que tiene las funciones)
 
-# Uso del alias Operacion
-
+"""
 # Llamado de las funciones del Ejercicio15
-
+# Uso del alias Operacion
 print(Operacion.suma(2,2))
 print(Operacion.resta(3,3))
 print(Operacion.multi(7,7))
