469d9a3d126fd819b52813290d4194035af7d0b1
Joy <joy.nelaton@utp.ac.pa>
2024-03-22 11:10:16 -0500

    Commit_79

---
+++ Ejercicio15.py
@@ -0,0 +1,18 @@
+#Archivo Modulo
+
+def suma (num1, num2):
+    result = num1 + num2
+    return result
+
+def resta (num1, num2):
+    result = num1 - num2
+    return result
+
+def multi (num1, num2):
+    result = num1 * num2
+    return result
+
+def divi (num1, num2):
+    result = num1 / num2
+    return result
+
---
+++ Ejercicio15B.py
@@ -0,0 +1,10 @@
+#Archivo principal (empleado para llamar al modulo con las funciones)
+
+import Ejercicio15   # Importe del Ejercicio15 que es el archivo modulo (aquel que tiene las funciones)
+
+# Llamado de las funciones del Ejercicio15
+
+print(Ejercicio15.suma(2,2))
+print(Ejercicio15.resta(3,3))
+print(Ejercicio15.multi(7,7))
+print(Ejercicio15.divi(10,5))
