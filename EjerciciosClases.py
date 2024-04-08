ba3d06bb61b6f3ece512b2f4889dd9b4216abcb9
joysg1 <joy.nelaton@utp.ac.pa>
2024-04-07 19:03:17 -0500

    Commit_108

--- EjerciciosClases.py
+++ EjerciciosClases.py
@@ -26,4 +26,27 @@ print("*"*80)
 
 print(miCompra.vender(2))
 print(miCompra.dar_cantidad())
+
+class ArticulosOficina(Articulo):
+    def __init__(self,desc,cantidad,coste):
+        self.desc = desc
+        self.cantidad = cantidad
+        self.coste = coste
+        self.venta = self.coste * self.cantidad
+    def __str__(self):
+        return f"Creado {self.desc}, {self.cantidad} con precio de {self.coste}"  
+    def devolver(self, unidades):
+        self.cantidad = self.cantidad + unidades
+        return f"Se han devuelto {unidades} de {self.desc} - Valor de devolucion = {self.coste * unidades}"
+ 
+print("\n")
+print("*"*80)    
+folder = ArticulosOficina("Folder",20,0.50)  
+print(folder)  
+print(folder.dar_cantidad()) 
+print(folder.vender(11)) 
+print(folder.dar_cantidad())
+print(folder.devolver(2))
+print(folder.dar_cantidad())
+        
    
