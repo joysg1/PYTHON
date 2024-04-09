e7002ea8b4e951addc6795b02b75787a4203e5d0
joysg1 <joy.nelaton@utp.ac.pa>
2024-04-08 20:03:07 -0500

    110

---
+++ EjercicioModuloCalc.py
@@ -0,0 +1,20 @@
+import ModuloCalc as cl
+
+
+suma = cl.suma(8,8)
+
+resta = cl.resta(2,2)
+
+multi = cl.multi(4,4)
+
+div = cl.div(5,0)
+
+div2 = cl.div(30,5)
+
+
+print(f"Suma = {suma}")
+print(f"Resta = {resta}")
+print(f"Multi = {multi}")
+print(f"Division = {div}")
+print(f"Division_2 = {div2}")
+
---
+++ ModuloCalc.py
@@ -0,0 +1,15 @@
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multi(num1, num2):
+    return num1 * num2
+
+def div(num1, num2):
+    if num2 == 0:
+        return "indeterminado"
+    else:
+        return num1 /num2
+        
