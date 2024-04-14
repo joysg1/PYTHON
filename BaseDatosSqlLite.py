dc75b9cc000e67fb0a508d949a14b1af5cd4bb35
joysg1 <joy.nelaton@utp.ac.pa>
2024-04-14 03:50:49 -0500

    Commit_115

--- BaseDatosSqlLite.py
+++ BaseDatosSqlLite.py
@@ -49,13 +49,44 @@ def consultar():
         print("ID = ", fila[0])
         print("Nombre ", fila[1])
         print("Telefono ", fila[2], '\n')
+    conexion.close()  
+    
+    
+def modificar(id, telefono):
+    conexion, cursor = conectar()
+    sql = f" UPDATE agenda SET telefono = {telefono}  WHERE ID = {id} "
+    cursor.execute(sql)
+    cursor.close()
+    conexion.commit()
+                               
+                    
+    
+    conexion.close()
+    
+    
+def borrar(id):
+    conexion, cursor = conectar()
+    sql = f"DELETE from agenda WHERE ID = {id}"
+    cursor.execute(sql)
+    cursor.close()
+    conexion.commit()
     conexion.close()    
+    
+"""          
 
 crearTabla()
 datos = "Luis", "6543-2188"
 datos2 = "Jose", "6457-9821"
+datos3 = "Hagi" , "6234-8711"
 insertar(datos)
 insertar(datos2)
+insertar(datos3)
+
+consultar()
+modificar(2,1111)
+
+"""
+
 
 consultar()
 
