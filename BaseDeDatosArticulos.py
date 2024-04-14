55cc1c1df9a2713bd75d75abd6e76922d852e131
joysg1 <joy.nelaton@utp.ac.pa>
2024-04-14 18:46:57 -0500

    Commit_116

---
+++ BaseDeDatosArticulos.py
@@ -0,0 +1,98 @@
+import sqlite3
+
+def conectar():
+    conexion = sqlite3.connect('articulos.db')
+    cursor = conexion.cursor()
+    print('Conectado a la base de datos')
+    return conexion, cursor
+
+def cerrar_conexion(conexion):
+    conexion.close()
+    
+
+def crear_tabla():
+    conexion, cursor = conectar()
+    cursor.execute('CREATE TABLE IF NOT EXISTS ARTICULOS (IDENTIFICADOR INT PRIMARY KEY, NOMBRE VARCHAR(20), CANTIDAD INT, IMPORTE FLOAT)')
+    cerrar_conexion(conexion)
+    print("Tabla creada")
+    
+def insertar(articulo):
+    conexion, cursor = conectar()
+    cursor.execute('INSERT INTO ARTICULOS VALUES (?,?,?,?)',articulo)
+    conexion.commit()
+    cerrar_conexion(conexion) 
+    
+    
+def consultar(): 
+    conexion, cursor = conectar()  
+    cursor.execute('SELECT * FROM articulos')
+    articulos = cursor.fetchall()
+    cerrar_conexion(conexion) 
+    print("CONSULTA REALIZADA") 
+    return articulos
+
+def actualizar(identificador, nombre, cantidad, importe):
+    conexion, cursor = conectar() 
+    
+    cursor.execute(f" UPDATE ARTICULOS SET nombre = '{nombre}' , cantidad = {cantidad} , importe = {importe} WHERE IDENTIFICADOR = {identificador}")
+    print("Articulo actualizado")
+    conexion.commit()
+    cerrar_conexion(conexion) 
+    
+    
+def borrar(identificador):
+    conexion, cursor = conectar() 
+    cursor.execute(f"DELETE FROM ARTICULOS WHERE IDENTIFICADOR = {identificador}")
+    
+    conexion.commit()
+    print("Articulo borrado")
+    cerrar_conexion(conexion)   
+
+def carga_inicial():
+    conexion, cursor = conectar()
+    articulos = [
+        (1,'Cuaderno',20,2.36),
+        (2, 'Lapiz',10,0.50),
+        (3, 'Borrador',12,1.89),
+        (4, 'Boligrafo',5,2.30)  
+        
+    ] 
+    
+    cursor.executemany('INSERT INTO ARTICULOS VALUES (?,?,?,?)',articulos)
+    conexion.commit()
+    cerrar_conexion(conexion)
+ 
+if __name__=='__main__':    
+ 
+ crear_tabla() 
+
+
+ # carga_inicial()
+ 
+ 
+ 
+ """
+ articulo = (6, "Libreta",10,2.50)
+ insertar(articulo)
+ 
+ """
+ 
+
+# actualizar(1,'Cuaderno Grande',10,1.50)
+
+
+"""
+
+borrar(5)
+borrar(1)
+
+"""
+
+articulos = consultar() 
+
+print("Lista de los nombres de los articulos")
+for articulo in articulos:
+    print(articulo[1])
+ 
+
+
---
+++ EjercicioBaseDeDatos.py
@@ -0,0 +1,10 @@
+"""
+Crea una tabla llamada Alumnos que constará 
+de tres columnas: la columna id de tipo entero, 
+la columna nombre que será de tipo texto y 
+la columna notas de tipo entero. Usa funcion 
+para insertar y mostrar datos. Una vez creada la 
+tabla, tenéis que insertarle 3 datos mediante input dentro de un bucle.
+
+
+"""
