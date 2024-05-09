5d534b0cafd2d809d528c3881c992baeae4560a8
joysg1 <joy.nelaton@utp.ac.pa>
2024-05-08 21:27:15 -0500

    Commit_132

--- EjercicioReloj.py
+++ EjercicioReloj.py
@@ -13,8 +13,15 @@ class relojDigital():
         self.label_hora.pack(pady=20)
         self.btn_inicio_crono =tk.Button(ventana, text = "Iniciar Cronometro",command=self.iniciar_cronometro)
         self.btn_inicio_crono.pack()
-
-        #Min 8.21 Video 94
+        self.btn_detener_crono = tk.Button(ventana,text = "Detener Cronometro",command=self.detener_cronometro,state="disabled")
+        self.btn_detener_crono.pack()
+        
+        self.btn_configurar_alarma = tk.Button(ventana,text="Configurar Alarma",command=self.configurar_alarma)
+        self.btn_configurar_alarma.pack()
+        
+        self.btn_activar_alarma = tk.Button(ventana, text = 'Activar Alarma',command=self.activar_alarma, state = "disabled")
+        self.btn_activar_alarma.pack()
+        #Min 12.24 Video 94
 
 
 
