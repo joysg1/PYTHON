e3ac824b4b6c5556935e42b2b264a3aac88ea80d
joysg1 <joy.nelaton@utp.ac.pa>
2024-05-14 22:26:54 -0500

    Commit_136

--- datosA/datosPruebas.py
+++ datosA/datosPruebas.py
@@ -2,6 +2,23 @@
 
 from faker import Faker
 
-dator = {}  
 
-#video 95 min 4.10
+
+
+
+def crear_datos(cantidad):
+    datos = {}  
+    datos_ficticios = Faker()
+
+    for i in range(cantidad):
+     nombre = datos_ficticios.name()
+     direccion = datos_ficticios.address()
+     datos[nombre]=direccion
+ 
+    return datos    
+
+if __name__=='__main__':
+    print(crear_datos(3))
+   
+#Video 95 min 8.15 
+    
