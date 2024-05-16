85b2f436c931a0b92b1d0ed92f372ac32f3b53b7
joysg1 <joy.nelaton@utp.ac.pa>
2024-05-15 22:01:16 -0500

    Commit_138

--- datosA/datosPruebas.py
+++ datosA/datosPruebas.py
@@ -1,6 +1,7 @@
 #pip install faker
 
 from faker import Faker
+import pandas as pd
 
 
 
@@ -15,11 +16,23 @@ def crear_datos(cantidad):
      direccion = datos_ficticios.address()
      datos[nombre]=direccion
  
-    return datos    
+    return datos   
+
+def generar_df(diccionario):
+    df =  pd.DataFrame([[clave, diccionario [clave]] for clave in diccionario.keys()], columns=['Nombre', 'Direccion'])
+    return df
+
+def crear_csv(df, fichero='datos'):
+    nombre_fichero = fichero + '.csv'
+    df.to_csv(nombre_fichero, encoding='utf-8', index=False)
+    
 
 if __name__=='__main__':
-    print(crear_datos(3))
-    print(crear_datos(5))
+    
+    datos = crear_datos(100) 
+    df = generar_df((datos))
+    print(df.head())
+    crear_csv(df,"pruebas")
    
 #Video 95 min 8.15 
     
