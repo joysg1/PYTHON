f0495f72df38e3571967f73b2f78923bd568193a
joysg1 <joy.nelaton@utp.ac.pa>
2024-05-16 23:40:02 -0500

    Commit_139

---
+++ IntroNumpy.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+list = [1,2,3,4,5]
+array = np.array(list)
+print(type(array))
+print(array)
+print(list)
+
+list2 = [[1,2,3,4,5],[6,7,8,9,10]]
+array2 = np.array(list2)
+print(array2)
+
+array3= np.arange(2,12,2)
+print(array3)
+
+#Video 96 minuto 9
