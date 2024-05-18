410ff0f2ba4b0d839337234a49dfc5cb71782c16
joysg1 <joy.nelaton@utp.ac.pa>
2024-05-17 23:37:37 -0500

    Commit_140

--- IntroNumpy.py
+++ IntroNumpy.py
@@ -14,4 +14,27 @@ print(array2)
 array3= np.arange(2,12,2)
 print(array3)
 
-#Video 96 minuto 9
+matrizCeros = np.zeros((4,5))
+print(matrizCeros)
+matrizUnos = np.ones((3, 3))
+print(matrizUnos)
+
+matriz = np.linspace(2, 6, 40)
+print(matriz)
+
+matrizIdentidad = np.eye(7)
+print(matrizIdentidad)
+
+matrizRandom = np.random.random((2,3))
+print(matrizRandom)
+
+matrizAleatoriaNormal = np.random.randn(5)
+print(matrizAleatoriaNormal)
+
+matrizAleatoriaEnteros = np.random.randint(1, 51, 20)
+print(matrizAleatoriaEnteros)
+
+arrayA = np.random.randint(1,201,30)
+print(arrayA)
+
+#Video 96 minuto 20
