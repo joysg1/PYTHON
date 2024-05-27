a21176b7816820bb5fe22b943532fa3039c7650f
joysg1 <joy.nelaton@utp.ac.pa>
2024-05-26 21:53:02 -0500

    Commit_146

--- PracticaPandas.py
+++ PracticaPandas.py
@@ -24,4 +24,33 @@ serie4 = serie1 - serie2
 print("\n")
 print(serie4)
 
-#Video 97 min 10:30
+filas = ['tienda01','tienda02','tienda03','tienda04']
+columnas =['articulo1','articulo2','articulo3']
+datos = [[224,200,100],[100,120,101],[50,100,101],[120,101,106]]
+
+dataframe = pd.DataFrame(datos, index=filas, columns=columnas)
+print(dataframe)
+
+# por fila
+print(dataframe.loc['tienda02'])
+print(dataframe.loc[['tienda01','tienda02']])
+
+#por columna
+print("\n")
+print(dataframe['articulo3'])
+
+#volor concreto
+print("\n")
+print(dataframe.loc['tienda01','articulo1'])
+
+#nueva columna
+dataframe['articulo4'] = [100,200,300,400]
+print("\n")
+print(dataframe)
+print("\n")
+dataframe['total']=dataframe['articulo1']+dataframe['articulo2']+dataframe['articulo3']+dataframe['articulo4']
+print(dataframe)
+
+# Video 97 min 21
+
+
