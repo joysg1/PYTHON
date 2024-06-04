39b99d6de8ad56f03c10880d9e1121e392acdf2d
joysg1 <joy.nelaton@utp.ac.pa>
2024-06-03 21:43:21 -0500

    Commit_152

--- MATPLOP/PracticaMatplop.py
+++ MATPLOP/PracticaMatplop.py
@@ -12,6 +12,52 @@ plt.title('Mi grafica')
 plt.xlabel('Eje x')
 plt.ylabel('Eje y')
 plt.show()
+plt.subplot(1,2,1)
+plt.plot(x, y, "red")
+plt.subplot(1, 2, 2)
+plt.plot(x, y, "green")
+plt.show()
 
 
-# Video 98 min 5
+plt.style.use('_mpl-gallery')
+
+# make data:
+np.random.seed(10)
+D = np.random.normal((3, 5, 4), (0.75, 1.00, 0.75), (200, 3))
+
+# plot:
+fig, ax = plt.subplots()
+
+vp = ax.violinplot(D, [2, 4, 6], widths=2,
+                   showmeans=False, showmedians=False, showextrema=False)
+# styling:
+for body in vp['bodies']:
+    body.set_alpha(0.9)
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
+
+
+from matplotlib import cm
+
+plt.style.use('_mpl-gallery')
+
+# Make data
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+# Plot the surface
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Blues)
+
+ax.set(xticklabels=[],
+       yticklabels=[],
+       zticklabels=[])
+
+plt.show()
+
+
