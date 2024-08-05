ef017e3241a30dbd7cff1d98a5f57ab532e9ecad
Joy <joy@gmail.com>
2024-08-05 13:16:05 -0500

    Commit_195

--- ventana.py
+++ ventana.py
@@ -1,25 +1,20 @@
-from tkinter import *
+import tkinter as tk
+import time
+class Ventana():
+    def __init__(self):
+        self.ventana = tk.Tk()
+        self.ventana.geometry("640x480")
+        self.ventana.resizable(0,0)
+        self.ventana.title("Reloj")
+        self.ventana.config(background="blue")
+        self.etiqueta = tk.Label(text="",font=("Arial",70),fg="blue",bg="white",pady=15,padx=15)
+        self.etiqueta.place(x=190,y=150)
+        self.actualizacion()
+        self.ventana.mainloop()
+    def actualizacion(self):
+        hora = time.strftime("%H:%M:%S")  
+        self.etiqueta.configure(text=hora) 
+        self.ventana.after(1000, self.actualizacion) 
 
-def click():
-    texto = "Hola , "+entrada.get()
-    etiqueta.configure(text=texto)
-
-ventana = Tk()
-
-ventana.title("Mi primera ventana con python")
-
-ventana.resizable(True,True)
-ventana.geometry("640x480")
-ventana.config(bg="green")
-frame = Frame()
-frame.pack()
-frame.config(bg="yellow")
-frame.config(width=640,height=350)
-etiqueta = Label(frame, text="Etiqueta")
-
-etiqueta.grid(column=1,row=2)
-entrada = Entry(frame, width = 50)
-entrada.grid(column=2, row=2)
-boton = Button(frame, text = "Pulsame",background="red", foreground="yellow",command = click)
-boton.grid(column = 1, row = 4)
-ventana.mainloop()
+if __name__=="__main__":
+    main = Ventana()
