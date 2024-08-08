d097d97690412696bfb1e7c6f2d5faa015b442b3
joysg1 <joy.nelaton@utp.ac.pa>
2024-08-08 16:22:11 -0500

    Commit_202

--- Ejercicio135Pandas.py
+++ Ejercicio135Pandas.py
@@ -28,7 +28,38 @@ print(df2)
 
 #4 Inspeccion y limpieza de datos
 
-# -1:21:56  url: https://codigofacilito.com/videos/clase-completa-pyhton-para-ciencias-de-datos-pandas
+
+print(df2.head()) # Primeros 5 registros
+print(df2.tail()) # Ultimos 5 registros
+print(df2.shape) # Total de registros y columnas
+print(df2.dtypes) # Tipo de datos de cada columna
+print(df2.describe()) # Proporciona estadisticas descriptivas de las columnas numericas
+print(df2.count()) #Cuenta el numero de valores no nulos en cada columna
+print(df2.info()) # Informacion general sobre el dataframe
+a = df2['Nombre'].unique() # Valores unicos de una columna
+print(a)
+df2.dropna() # Elimina filas con valores nulos
+df2.drop_duplicates(inplace=True) # Elimina filas duplicadas
+df2.fillna(value=0) # Rellena los valores nulos con 0
+df2['Edad'] = df2['Edad'].astype('int') # Cambia el tipo de dato de una columna
+# df2.rename(columns={'columna1': 'nuevo_nombre'}, inplace=True) # Cambia el nombre de una columna
+
+
+
+# 5 Manipulacion de datos
+
+# Seleccion y filtrado
+
+print(df2['Nombre']) # Selecciona una columna
+print(df2[df2['Edad'] > 30]) # Filtra por una condicion
+print(df2[(df2['Edad'] > 30) & (df2['Ciudad'] == 'Madrid')]) # Filtra por multiples condiciones
+print(df2.groupby('Ciudad').agg({'Edad': 'sum'})) # Agrupa por una columna y aplica una funcion
+
+
+
+
+
+ # https://codigofacilito.com/videos/clase-completa-pyhton-para-ciencias-de-datos-pandas
 
 
 
