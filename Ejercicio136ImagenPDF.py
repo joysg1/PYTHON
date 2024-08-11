def95979228b4a3abaa28bfac7c742d02c504cb9
joysg1 <joy.nelaton@utp.ac.pa>
2024-08-11 16:12:58 -0500

    Commit_2026

---
+++ Ejercicio136ImagenPDF.py
@@ -0,0 +1,24 @@
+import fitz
+import os 
+
+def agregar_imagen(pdf_original, pdf_final, imagen_png, x, y, ancho, alto):
+    pdf_documento = fitz.open(pdf_original)
+    imagen = fitz.open(imagen_png)
+    pagina = pdf_documento[0]
+    img_rect = fitz.Rect(x, y, x + ancho, y + alto)
+    pagina.insert_image(img_rect, pixmap=imagen[0].get_pixmap(alpha=True))
+    pdf_documento.save(pdf_final)
+    pdf_documento.close()
+    imagen.close()
+    
+if __name__ == "__main__":
+    pdf_original = "CARTA.pdf"
+    pdf_final = "CARTA_final.pdf"
+    imagen_png = "FIRMA.png"
+    x = 110
+    y = 440
+    ancho = 90
+    alto = 90
+    agregar_imagen(pdf_original, pdf_final, imagen_png, x, y, ancho, alto)
+    os.rename(pdf_final, "CARTA_final_renombrada.pdf")
+
