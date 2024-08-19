b4897fd503ac3d1f148010300579b825300e3c5f
Joy <joy.nelaton@utp.ac.pa>
2024-08-19 14:24:25 -0500

    Commit_220

---
+++ Ejercicio147_Listas.py
@@ -0,0 +1,16 @@
+Lista = []
+Lista.append(1)
+Lista.append("Carro")
+Lista.append(0.25)
+Lista.append(True)
+
+print(Lista)
+print(type(Lista))
+
+# Las listas son tipos de datos mutables, es decir, pueden ser modificadas
+# Las listas pueden contener elementos de diferentes tipos de datos
+# Las listas pueden ser anidadas, es decir, una lista puede contener otra lista
+
+Lista2 = [1, "Carro", 0.25, True, [1, 2, 3, 4]]
+print(Lista2)
+print(type(Lista2))
