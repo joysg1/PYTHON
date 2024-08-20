14be51f03ca450ba3630ee4cb8ed31624f596257
Joy <joy.nelaton@utp.ac.pa>
2024-08-20 12:25:27 -0500

    Commit_223

---
+++ Ejercicio156_Intercambio_Valor_Variables.py
@@ -0,0 +1,10 @@
+# Intercambio de valores entre dos variables sin usar variable temporal
+a = int(input("Ingrese el valor de a: "))
+b = int(input("Ingrese el valor de b: "))
+print("-"*80)
+print("Valor original de a: ",a)
+print("Valor original de b: ", b)
+a, b = b, a
+print("-"*80)
+print("Valor nuevo de a: ", a)
+print("Valor nuevo de b: ", b)
