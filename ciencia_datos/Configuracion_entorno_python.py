785ef5c76cc2b416ca14aae8315650753bb72c0d
joysg1 <joy.nelaton@utp.ac.pa>
2025-01-07 04:27:01 -0500

    Add files via upload

---
+++ ciencia_datos/Configuracion_entorno_python.py
@@ -0,0 +1,14 @@
+# Para crear el entorno
+
+# python -m venv <nombre_entono>
+
+# Se creara una carpeta con el nombre del entorno, verla con 
+# el comando ls (Linux) o cd (Windows)
+
+# Para activar el entorno
+
+# source <nombre_entorno>/bin/activate
+
+
+# Quede en el minuto -33:53
+
