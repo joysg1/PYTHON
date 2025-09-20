0b0ace4fd7e711a609caf78865cf5d76e3468470
joysg1 <joy.nelaton@utp.ac.pa>
2025-09-19 19:47:30 -0500

    Update buscador_dominios.py

--- buscador_dominios.py
+++ buscador_dominios.py
@@ -1,1 +1,14 @@
+import requests
+import sys
 
+sub_list = open("worldlist.txt").read()
+subdomains = sub_list.splitlines()
+
+for sub in subdomains:
+  sub_domain = f"http://{sub}.{sys.argv[1]}"
+  try:
+    requests.get(sub_domain)
+  except requests.ConnectionError:
+    pass
+  else:
+    print("Found: ",sub_domain)
