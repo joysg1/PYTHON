58a18afe2eca7782389590bc6ebfca2b3a5c2299
joysg1 <joy.nelaton@utp.ac.pa>
2025-09-19 20:23:40 -0500

    Update ports.py

--- ports.py
+++ ports.py
@@ -1,1 +1,31 @@
+import sys
+import socket
 
+ip = "192.168.1.6"
+open_ports=[]
+
+ports = range[1,65535]
+
+def probe_port(ip, port, result = 1):
+  try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.6)
+    r= sock.connect_ex((ip, port))
+    if r ==0:
+      result = r
+    sock.close()
+  except Exception as e:
+    pass
+  return result
+
+for port in ports:
+  sys.stdout.flush()
+  response = probe_port(ip, port)
+  if response == 0:
+    open_ports.append(port)
+
+if open_ports:
+  print("Open Ports are: ")
+
+
+// Nota usando nmap <ip_a_escanear> obtenemos el mismo resultado
