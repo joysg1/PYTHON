9bb893c5eb53111b802d1719eb0805abb8ea61c8
joysg1 <joy.nelaton@utp.ac.pa>
2025-09-19 19:56:12 -0500

    Update directory_scan.py

--- directory_scan.py
+++ directory_scan.py
@@ -1,1 +1,14 @@
+import requests
+import sys
 
+sub_list = open("worldlist.txt").read()
+directories = sub_list.splitlines()
+
+for dir in directories:
+  dir_enum = f "http://{sys.argv[1]}/{dir}.html"
+  r = requests.get(dir.enum)
+  if r.status_code==404:
+    pass
+  else:
+    print("Valid directory: ",dir_enum)
+    
