82f77d4a93463bcd8e7acb8e947387aa03a45d28
joysg1 <joy.nelaton@utp.ac.pa>
2025-09-25 20:10:51 -0500

    Add files via upload

--- CARS_MACHINE_LEARNING/Analysis.py
+++ CARS_MACHINE_LEARNING/Analysis.py
@@ -12,13 +12,61 @@ print(df_cars.head())
 print(df_cars.describe())
 print(df_cars.info())
 print(df_cars.isnull().sum())
-# Codigo para verificar el tipo de dato de cada columna
-print(df_cars.dtypes)
+
 # Codigo para encontrar la media de cada columna
 print(df_cars.mean())
+
 # Codigo para modificar los valores de la columna gender y aplicar donde sea 0 por masculino y donde sea 1 por femenino
 df_cars['gender'] = df_cars['gender'].replace(0, 'Masculino')
 df_cars['gender'] = df_cars['gender'].replace(1, 'Femenino')
 print(df_cars.head())
+print(df_cars.dtypes)
+
+# Codigo para cambiar el tipo de dato de sales a float
+df_cars['sales'] = df_cars['sales'].astype(float)
+
+# Codigo para cambiar el tipo de dato de income a float
+df_cars['income'] = df_cars['income'].astype(float)
+
+# Codigo para cambiar el tipo de dato de miles a float
+df_cars['miles'] = df_cars['miles'].astype(float)
+
+# Codigo para cambiar el tipo de dato de debt a float
+df_cars['debt'] = df_cars['debt'].astype(float)
+
+# Codigo para verificar el tipo de dato de cada columna
+print(df_cars.dtypes)
+
+# Codigo para ver el tamaño del dataframe
+print(df_cars.shape)
+
+# Codigo para ver la cantidad de filas del dataframe
+print(len(df_cars))
+
+# Codigo para ver la cantidad de columnas del dataframe
+print(len(df_cars.columns))
+
+# Codigo para ver las descripciones de cada columna
+print(df_cars.describe())
+
+# Codigo para cambiar los valores que sean 2 en gender por la cadena genero no especificado
+df_cars['gender'] = df_cars['gender'].replace(2, 'Genero no especificado')
+
+# Codigo para ver los valores unicos por columna
+print(df_cars.nunique())
+
+# Codigo para ver la cantidad de masculinos y femeninos
+print(df_cars['gender'].value_counts())
+
+
+# Codigo para crear un dataframe de solo los de genero femenino
+df_cars_femenino = df_cars[df_cars['gender'] == 'Femenino']
+
+print(df_cars_femenino.head())
+
+# Codigo para imprimir la media de edad de los de genero femenino
+print(df_cars_femenino['age'].mean())
+
+
 
 
